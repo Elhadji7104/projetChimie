020f4851bd4ba39d354acb570ec57732b859926e
Dairou Sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2019-04-29 00:36:16 +0200

    mise à jour fiche detail

--- src/main/webapp/app/entities/fiche-article/fiche-article-detail.component.ts
+++ src/main/webapp/app/entities/fiche-article/fiche-article-detail.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IFicheArticle } from 'app/shared/model/fiche-article.model';
+import { IDroitDacceeProduit } from 'app/shared/model/droit-daccee-produit.model';
+import { AccountService } from 'app/core';
+import { DroitDacceeProduitService } from '../droit-daccee-produit';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-fiche-article-detail',
@@ -9,13 +14,24 @@ import { IFicheArticle } from 'app/shared/model/fiche-article.model';
 })
 export class FicheArticleDetailComponent implements OnInit {
     ficheArticle: IFicheArticle;
-
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    droitDacceeProduits: IDroitDacceeProduit[] = [];
+    constructor(
+        protected jhiAlertService: JhiAlertService,
+        protected droitDacceeProduitService: DroitDacceeProduitService,
+        protected activatedRoute: ActivatedRoute
+    ) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ ficheArticle }) => {
             this.ficheArticle = ficheArticle;
         });
+        this.droitDacceeProduitService.query().subscribe(
+            (res: HttpResponse<IDroitDacceeProduit[]>) => {
+                this.droitDacceeProduits = res.body.filter(droit => droit.ficheArticle.codeBarre === this.ficheArticle.codeBarre);
+                console.log(this.ficheArticle);
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
     }
 
     previousState() {
@@ -23,5 +39,9 @@ export class FicheArticleDetailComponent implements OnInit {
     }
     gotoLien(lien: string) {
         window.location.href = lien;
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 }
