c38197bee77a018c4fcaee251e67d12cb1780bd2
Dairou Sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2019-04-29 16:05:59 +0200

    suppression fiche article tableau

--- src/main/webapp/app/entities/fiche-article/index.ts
+++ src/main/webapp/app/entities/fiche-article/index.ts
@@ -2,5 +2,4 @@ export * from './fiche-article.service';
 export * from './fiche-article-update.component';
 export * from './fiche-article-delete-dialog.component';
 export * from './fiche-article-detail.component';
-export * from './fiche-article.component';
 export * from './fiche-article.route';
