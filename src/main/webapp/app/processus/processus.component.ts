1f669d00a346a95d600a384680968b32cc5b4c7b
Dairou Sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2019-05-08 15:49:13 +0200

    amélioration de la page edit processus

--- src/main/webapp/app/processus/processus.component.ts
+++ src/main/webapp/app/processus/processus.component.ts
@@ -79,9 +79,23 @@ export class ProcessusComponent implements OnInit {
 
     ngOnInit() {
         this.isSaving = false;
+
         this.activatedRoute.data.subscribe(({ ficheArticle }) => {
-            console.log(ficheArticle);
-            //this.ficheArticle = ficheArticle;
+            if (ficheArticle) {
+                this.ficheArticle = ficheArticle;
+                console.log(ficheArticle);
+
+                this.ficheProduits = ficheArticle.ficheProduitChimiques;
+                if (ficheArticle.documents[0]) {
+                    this.documentInput = ficheArticle.documents[0].lien;
+                }
+                if (ficheArticle.unites[0]) {
+                    this.uniteArray = ficheArticle.unites[0].libelleUnite;
+                }
+                this.classiArray = ficheArticle.classifications;
+                this.droit = ficheArticle.droitDacceeProduits;
+                this.typeCond = ficheArticle.typeDeConditionnements;
+            }
         });
 
         this.groupeService.query().subscribe(
